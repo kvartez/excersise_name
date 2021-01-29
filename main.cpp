fe87e29df863b9562de355b11c4a0a01ac327049
kvartez <38496672+kvartez@users.noreply.github.com>
2021-01-29 10:31:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 using namespace std;
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 int N = 5;
 struct uczen
 {
@@ -10,8 +9,8 @@ struct uczen
 };
 
 int main(int argc, char** argv) {
-	uczen t[5];
-	for(int i =0; i<5;i++){
+	uczen t[N];
+	for(int i =0; i<N;i++){
 		cout <<i<< endl;
 		cout<< "podaj imie";
 		cin >> t[i].imie;
@@ -20,16 +19,18 @@ int main(int argc, char** argv) {
 		cout<< "podaj numer";
 		cin >> t[i].numer;
 		
-	}
+	}	
+	
 	cout<< "id";
 	cout<< "\t imie";
 	cout<< "\t nazwisko";
 	cout<< "\t numer"<<endl;
-	for(int i =0; i<5;i++){
-	cout << i;
-	cout<< "\t "<<t[i].imie;
-	cout<< "\t "<<t[i].nazwisko;
-	cout<< "\t\t"<<t[i].numer<<endl;
+	
+	for(int i =0; i<N;i++){
+		cout << i;
+		cout<< "\t "<<t[i].imie;
+		cout<< "\t "<<t[i].nazwisko;
+		cout<< "\t\t"<<t[i].numer<<endl;
 	}
 	
 	return 0;
